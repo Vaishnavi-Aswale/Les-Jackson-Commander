fe6e7054494b2a5229bcc60087d0211b29b080b7
Ben Ferdinandus <benferdinandus@uitgeverij-deviant.nl>
2020-11-13 20:24:08 +0100

    Initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repo;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            IEnumerable<Command> commandItems = _repo.GetAllCommands();
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            Command commandItem = _repo.GetCommandById(id);
+
+            if (commandItem != null) {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto cmd)
+        {
+            Command commandModel = _mapper.Map<Command>(cmd);
+
+            _repo.CreateCommand(commandModel);
+            _repo.SaveChanges();
+
+            return Created($"/api/commands/{commandModel.Id}", _mapper.Map<CommandReadDto>(commandModel));
+        }
+    }
+}
---
+++ Data/SqlCommanderRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext _context;
+
+        public SqlCommanderRepo(CommanderContext context)
+        {
+            _context = context;
+        }
+
+        public bool SaveChanges() => _context.SaveChanges() >= 0;
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Commands.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null) {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Add(cmd);
+        }
+    }
+}
